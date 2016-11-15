2a4ec263ce1e801ad56a57b8b6cc8458f9c56150
jenmcphail <mcphail.jen@gmail.com>
2016-11-15 11:14:58 -0800

    removes angular 2 components in app folder

--- app/main.js
+++
@@ -1,6 +0,0 @@
-(function(app) {
-  document.addEventListener('DOMContentLoaded', function() {
-    ng.platformBrowserDynamic
-      .platformBrowserDynamic()
-  });
-})(window.app || (window.app = {}));
